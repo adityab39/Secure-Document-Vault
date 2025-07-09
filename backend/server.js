af7acc955158a903a4acd9b5cf37e7d9cafea4c1
atharva-git06 <chavanatharva223@gmail.com>
2025-07-09 19:24:18 -0400

    added upload to s3 functionality changes port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const s3Routes = require("./routes/s3-router")
 require('dotenv').config();  // To use environment variables from .env file
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
